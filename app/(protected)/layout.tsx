00f8d1768fd7ccc1a167691c4ac5e47a82d000a3
Vlad <v.g.iftode@student.tudelft.nl>
2025-04-20 17:34:37 +0300

    idk

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,12 +1,9 @@
-"use client"
-
 import type React from "react"
 import { MobileNav } from "@/components/mobile-nav"
 import { DrinkModal } from "@/components/modals/drink-modal"
 import { redirect } from "next/navigation"
 import { getServerSession } from "next-auth/next"
 import { authOptions } from "@/lib/auth"
-import { Suspense } from "react"
 
 export default async function ProtectedLayout({
   children,
@@ -21,13 +18,9 @@ export default async function ProtectedLayout({
 
   return (
     <div className="flex min-h-screen flex-col pb-16 md:pb-0">
-      <Suspense fallback={<div>Loading...</div>}>
-        {children}
-      </Suspense>
-      <Suspense fallback={null}>
-        <MobileNav />
-        <DrinkModal />
-      </Suspense>
+      {children}
+      <MobileNav />
+      <DrinkModal />
     </div>
   )
 }
